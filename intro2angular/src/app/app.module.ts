dc19458b01f274a9a4a8b643e05979af8f552657
cheikhomarsow <cheikh-omar.sow@supinternet.fr>
2018-07-02 20:30:02 +0200

    login App part 2

--- intro2angular/src/app/app.module.ts
+++ intro2angular/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { DataComponent } from './data/data.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,7 @@ import { AdminComponent } from './admin/admin.component';
       }
     ])
   ],
-  providers: [RecordsService],
+  providers: [RecordsService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- intro2angular/src/app/auth.service.ts
+++ intro2angular/src/app/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable()
 
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getUsersDetails() {
+  getUserDetails(username, password) {
+    console.log(username, password)
     //post these details to API server return user info if correct
+    return this.http.post('/api/auth.php', {
+      username,
+      password
+    })
   }
 }
