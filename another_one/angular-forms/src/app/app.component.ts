f88e4ecf784bf23621e16c4a8539b802bc626597
cheikhomarsow <cheikh-omar.sow@supinternet.fr>
2018-07-05 15:37:33 +0200

    Form Validation

--- another_one/angular-forms/src/app/app.component.ts
+++ another_one/angular-forms/src/app/app.component.ts
@@ -8,8 +8,16 @@ import { User } from './user';
 })
 export class AppComponent {
   topics = ['Angular', 'React','Vue'];
+  topicHasError = true;
 
-  userModel = new User('Cheikh', 'cheikhomar60@gmail.com', 2020202, '', 'morning', true);
+  userModel = new User('Cheikh', 'cheikhomar60@gmail.com', 2020222202, 'default', 'morning', true);
 
+  validateTopic(value){
+    if(value == 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false
+    }
+  }
   
 }
